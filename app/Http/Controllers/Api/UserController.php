33c44f741fdc65e0554f12caf7cc1ef765d25f31
ZulfikarHD <zulfikar.h9050@gmail.com>
2025-03-19 18:04:13 +0700

    Feature: Create CRUD function in Api/User Controller and setup the api Route

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,8 +4,39 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function getAllUsers()
+    {
+        $users = User::select('id', 'name', 'email', 'age')->get();
+        return response()->json($users);
+    }
+
+    public function getUserById($id)
+    {
+        $user = User::select('id', 'name', 'email', 'age')->find($id);
+        return response()->json($user);
+    }
+
+    public function createUser(Request $request)
+    {
+        $user = User::create($request->all());
+        return response()->json($user);
+    }
+
+    public function updateUser(Request $request, $id)
+    {
+        $user = User::find($id);
+        $user->update($request->all());
+        return response()->json($user);
+    }
+
+    public function deleteUser($id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return response()->json(['message' => 'User deleted successfully']);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::get('/users', [UserController::class, 'getAllUsers']);
+Route::get('/users/{id}', [UserController::class, 'getUserById']);
+Route::post('/users', [UserController::class, 'createUser']);
+Route::put('/users/{id}', [UserController::class, 'updateUser']);
+Route::delete('/users/{id}', [UserController::class, 'deleteUser']);
